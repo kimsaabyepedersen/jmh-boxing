39de5be4754c0f74eac2c32af3949efbd8659597
Kim Saabye Pedersen <k.epost@gmail.com>
2021-12-18 09:40:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java JMH boxing
+[Read more here](https://www.anno1980.dk/posts/java-boxing-benchmark/)
---
+++ src/main/java/dk/anno1980/BoxingBenchmark.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package dk.anno1980;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(3)
+@Warmup(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
+public class BoxingBenchmark {
+
+  @Param({"0", "200"})
+  int startValue;
+
+  int[] ints;
+  Integer[] integers;
+  int count = 128;
+
+  @Setup
+  public void setup() {
+    ints = new int[count];
+    integers = new Integer[count];
+  }
+
+  @Benchmark
+  public Integer[] boxing() {
+    for (int i = 0; i < integers.length; i++) {
+      integers[i] = i + startValue;
+    }
+    return integers;
+  }
+
+  @Benchmark
+  public int[] nonBoxing() {
+    for (int i = 0; i < ints.length; i++) {
+      ints[i] = i + startValue;
+    }
+    return ints;
+  }
+}
